5afee2e4060fe42e391f075d5fe48a769291c69a
DmitryDanilin <tuning-garage@mail.ru>
2024-06-20 04:11:43 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ConverterPage from './pages/ConverterPage';
+import ExchangeRatesPage from './pages/ExchangeRatesPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ConverterPage />} />
+        <Route path="/rates" element={<ExchangeRatesPage />} />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
